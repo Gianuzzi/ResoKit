586e96c3a67a431b0874643c7ffc85ff2d894862
MatiasCerioni <matias.cerioni@unc.edu.ar>
2024-11-01 16:16:31 -0300

    blacked some files

--- resokit/core.py
+++ resokit/core.py
@@ -1,88 +1,106 @@
 import numpy as np
 import attrs
 
+
 # Custom validators
 def _validate_sequence(instance, attribute, value):
     if value is not None and not isinstance(value, (list, tuple, np.ndarray)):
-        raise TypeError(f"{attribute.name} must be a list, tuple, np.ndarray, \
-                        or None.")
+        raise TypeError(
+            f"{attribute.name} must be a list, tuple, np.ndarray, \
+                        or None."
+        )
+
 
 def _validate_constant(instance, attribute, value):
     if value is not None and not isinstance(value, (int, float)):
         raise TypeError(f"{attribute.name} must be an int, float, or None.")
 
+
 # Converter to ensure sequences are converted to numpy arrays if not None
 def _to_numpy_array(value):
     return np.array(value) if value is not None else None
 
+
 # Join validator and converter
-_TIME_SEQ_VALID_AND_CONV={
-    'validator':_validate_sequence,
-    'converter':_to_numpy_array}
+_TIME_SEQ_VALID_AND_CONV = {
+    "validator": _validate_sequence,
+    "converter": _to_numpy_array,
+}
+
 
 # Utility functions
 def _exists(var):
     return var is not None
 
+
 def _everything_exists(*variables):
-    return(all([_exists(var) for var in variables]))
-    
-#####--------------------- DynamicPlanet
+    return all([_exists(var) for var in variables])
+
+
+# ------------------------- DynamicPlanet
 @attrs.define(frozen=False, slots=True, repr=False)
 class DynamicPlanet:
     """Time series for the evolution of a planet"""
-    
+
     # Input time arrays
-    times: list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # yr
-    a:     list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # AU
-    e:     list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) #
-    inc:   list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # deg
-    M:     list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # deg
-    w:     list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # deg
-    Omega: list = attrs.field(default=None,**_TIME_SEQ_VALID_AND_CONV) # deg
-    
+    times: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # yr
+    a: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # AU
+    e: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  #
+    inc: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # deg
+    M: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # deg
+    w: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # deg
+    Omega: list = attrs.field(default=None, **_TIME_SEQ_VALID_AND_CONV)  # deg
+
     # Input constants
-    mass:   float = attrs.field(default=None,validator=_validate_constant)  # earth masses
-    radius: float = attrs.field(default=None,validator=_validate_constant)  # earth radii
-    
+    mass: float = attrs.field(
+        default=None, validator=_validate_constant
+    )  # earth masses
+    radius: float = attrs.field(
+        default=None, validator=_validate_constant
+    )  # earth radii
+
     # Flags
-    is_star: bool = attrs.field(default=False) # is_star flag
-    
+    is_star: bool = attrs.field(default=False)  # is_star flag
+
     # Additional info
-    name: str = attrs.field(default="", # planets name
-                            validator=attrs.validators.instance_of(str)) 
-    
+    name: str = attrs.field(
+        default="", validator=attrs.validators.instance_of(str)  # planets name
+    )
+
     # Calculated arrays
-    _varpi : list = attrs.field(init=False,
-                                default=None,
-                                validator=_validate_sequence) # mean longitude
-    _lam : list = attrs.field(init=False,
-                              default=None,
-                              validator=_validate_sequence) # mean longitude
-  
+    _varpi: list = attrs.field(
+        init=False, default=None, validator=_validate_sequence
+    )  # mean longitude
+    _lam: list = attrs.field(
+        init=False, default=None, validator=_validate_sequence
+    )  # mean longitude
+
     @property
     def varpi(self):
-        if not _everything_exists(self.w,self.Omega):
-            raise TypeError("'w' and 'Omega' are \
-                            required to calculate 'varpi'")
+        if not _everything_exists(self.w, self.Omega):
+            raise TypeError(
+                "'w' and 'Omega' are \
+                            required to calculate 'varpi'"
+            )
         elif self._varpi is None:
-            self._varpi=(self.w+self.Omega)%360
-        return(self._varpi)            
-    
+            self._varpi = (self.w + self.Omega) % 360
+        return self._varpi
+
     @property
     def lam(self):
-        if not _everything_exists(self.M,self.w,self.Omega):
-            raise TypeError("'M', 'w' and 'Omega' are\
-                            required to calculate 'varpi'")
+        if not _everything_exists(self.M, self.w, self.Omega):
+            raise TypeError(
+                "'M', 'w' and 'Omega' are\
+                            required to calculate 'varpi'"
+            )
         elif self._lam is None:
-            self._lam = (self.M+self.w+self.Omega)%360
-        return(self._lam)
-        
-    
+            self._lam = (self.M + self.w + self.Omega) % 360
+        return self._lam
+
     def planet_method(self): ...
 
 
-#####--------------------- STAR
+# ------------------------- STAR
 @attrs.define(repr=False)
 class Star:
     """
@@ -90,41 +108,62 @@ class Star:
 
     """
 
-    mass: float = attrs.field(default=None,validator=_validate_constant)  # solar masses
-    radius: float = attrs.field(default=None,validator=_validate_constant)  # solar radii
+    mass: float = attrs.field(
+        default=None, validator=_validate_constant
+    )  # solar masses
+    radius: float = attrs.field(
+        default=None, validator=_validate_constant
+    )  # solar radii
 
     def star_method(self): ...
 
 
 _LIST_OF_PLANETS_VALID = attrs.validators.deep_iterable(
     member_validator=attrs.validators.instance_of(DynamicPlanet)
-    )
+)
 
+
+# ------------------------- DynamicSystem
 @attrs.define(repr=False)
 class DynamicSystem:
     """Global properties of the system"""
 
-    nstars: int = attrs.field(default=1,
-                              validator=attrs.validators.instance_of(int))
-    star: Star = attrs.field(default=Star(),
-                             validator=attrs.validators.instance_of(Star))
-    planets: list = attrs.field(default=[],validator=_LIST_OF_PLANETS_VALID)
-    npl: int = attrs.field(init=False)
-    times: list = attrs.field(init=False,
-                              default=None,
-                              **_TIME_SEQ_VALID_AND_CONV)
+    # planets
+    planets: list = attrs.field(
+        default=[], 
+        validator=_LIST_OF_PLANETS_VALID
+        )
+    # star
+    star: Star = attrs.field(
+        default=Star(), 
+        validator=attrs.validators.instance_of(Star)
+    )
+    # parameters
+    npl: int = attrs.field(
+        init=False
+        )
+    nstars: int = attrs.field(
+        default=1, 
+        validator=attrs.validators.instance_of(int)
+        )
+    # time array
+    times: list = attrs.field(
+        init=False, 
+        default=None, 
+        **_TIME_SEQ_VALID_AND_CONV
+        )
 
     def __attrs_post_init__(self):
         self.times = self.planets[0].times
         self.npl = len(self.planets)
-        
+
     def prat(self):
         masses = np.asarray([pli.mass for pli in self.planets])
         if not all(masses):
             mu_l = 1
-            Warning('Assuming mass=0 for planets in calculating prat')
+            Warning("Assuming mass=0 for planets in calculating prat")
         else:
-            mu_l = (self.star.mass + masses) # neglect factor G because of ratios
-        nl = np.asarray([(mu_l/pli.a**3)**0.5 for pli in self.planets])
-        prat = nl[:-1]/nl[1:]
-        return(prat)
+            mu_l = self.star.mass + masses  # neglect factor G because of ratios
+        nl = np.asarray([(mu_l / pli.a**3) ** 0.5 for pli in self.planets])
+        prat = nl[:-1] / nl[1:]
+        return prat
--- resokit/io.py
+++ resokit/io.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import attrs
-from core import DynamicPlanet,Star,DynamicSystem
+from core import DynamicPlanet, Star, DynamicSystem
 
 # allowed inputs
 ELEM_SPACE = {
@@ -17,12 +17,13 @@ ELEM_SPACE = {
     "mass": None,
 }
 
-  
-def _my_get_key(df,key):
+
+def _my_get_key(df, key):
     if key in df.columns:
-        return(df[key].values) 
+        return df[key].values
     else:
         return None
+
 
 #####--------------------- LOAD INTEGRATION
 def load_integration(
@@ -35,7 +36,7 @@ def load_integration(
     plnames=None,
     is_star=None,
     st_m=None,
-    st_r=None
+    st_r=None,
 ):
     """
     Load integration from file.
@@ -48,8 +49,8 @@ def load_integration(
         Number of planets.
     names : list of str, optional
         Contents of the columns. Can only include the terms ["time", "ibody",
-        "a", "e", "inc", "M", "w", "Omega", "mass", "_"]. Use "_" for 
-        throwaways. The default is ["time", "ibody", "a", "e", "inc", "M", 
+        "a", "e", "inc", "M", "w", "Omega", "mass", "_"]. Use "_" for
+        throwaways. The default is ["time", "ibody", "a", "e", "inc", "M",
         "w", "Omega"].
     mass : list of floats, optional
         Planet masses in Earth masses.
@@ -64,7 +65,9 @@ def load_integration(
 
     # =============== VALIDATE PARAMETERS =============== #
     for namei in names:
-        assert (namei in ELEM_SPACE), f"{namei} is not an \
+        assert (
+            namei in ELEM_SPACE
+        ), f"{namei} is not an \
     allowed input"
 
     if (usecols is not None) and (len(names) != len(usecols)):
@@ -73,30 +76,29 @@ def load_integration(
     # correct use of mass
     if ("mass" in names) and (mass is not None):
         raise Exception("can't input mass twice")
-    
+
     # is_star flag
-    if is_star!=None:
-        if isinstance(is_star,(int,float)) and is_star>0:
-            is_star_list = [False]*npl
-            is_star_list[is_star-1] = True
+    if is_star != None:
+        if isinstance(is_star, (int, float)) and is_star > 0:
+            is_star_list = [False] * npl
+            is_star_list[is_star - 1] = True
             is_star = is_star_list
-        elif isinstance(is_star,(list,tuple,np.ndarray)) and (0 not in is_star):
-            is_star_list = np.array([False]*(npl+1))
-            is_star_list = [True if i in is_star else False \
-                            for i in range(npl+1)]
+        elif isinstance(is_star, (list, tuple, np.ndarray)) and (0 not in is_star):
+            is_star_list = np.array([False] * (npl + 1))
+            is_star_list = [True if i in is_star else False for i in range(npl + 1)]
             is_star = is_star_list[1:]
         else:
-            raise Exception('bad use of is_star')
-    
-    if plnames and len(plnames)!=npl:
-            raise ValueError('Shape of plnames mismatch')
-            
+            raise Exception("bad use of is_star")
+
+    if plnames and len(plnames) != npl:
+        raise ValueError("Shape of plnames mismatch")
+
     # =============== READ DATA =============== #
     # select parameters with usecols
     N_names = len(names)
     usecols = [i for i in range(N_names) if names[i] != "_"]
     names = [names[i] for i in usecols]
-    
+
     # read data
     data = pd.read_table(
         file,
@@ -124,38 +126,42 @@ def load_integration(
                             using an 'ibody' column or one file per planet"
             )
         data["ibody"] = pl_ibodies
-    
+
     # set default values of mass and radius to list of nones
-    if mass is None: mass = [None]*npl
-    if radius is None: radius = [None]*npl
-    if plnames is None: plnames = [""]*npl
-    if is_star is None: is_star = [False]*npl
-    
+    if mass is None:
+        mass = [None] * npl
+    if radius is None:
+        radius = [None] * npl
+    if plnames is None:
+        plnames = [""] * npl
+    if is_star is None:
+        is_star = [False] * npl
+
     # always use a list of masses
-    if ('mass' in names):
-        mass=data['mass'].values[:npl]
-        
+    if "mass" in names:
+        mass = data["mass"].values[:npl]
+
     # construct DynamicPlanets objects
     planets = []
     for ipl in range(npl):
         ith_pl = data[data["ibody"] == ipl + 1]
         ith_pl_obj = DynamicPlanet(
-            times = _my_get_key(ith_pl, 'times'),
-            a =     _my_get_key(ith_pl, 'a'),
-            e =     _my_get_key(ith_pl, 'e'),
-            inc =   _my_get_key(ith_pl, 'inc'),
-            M =     _my_get_key(ith_pl, 'M'),
-            w =     _my_get_key(ith_pl, 'w'),
-            Omega = _my_get_key(ith_pl, 'Omega'),
+            times=_my_get_key(ith_pl, "times"),
+            a=_my_get_key(ith_pl, "a"),
+            e=_my_get_key(ith_pl, "e"),
+            inc=_my_get_key(ith_pl, "inc"),
+            M=_my_get_key(ith_pl, "M"),
+            w=_my_get_key(ith_pl, "w"),
+            Omega=_my_get_key(ith_pl, "Omega"),
             mass=mass[ipl],
             radius=radius[ipl],
             name=plnames[ipl],
-            is_star=is_star[ipl]
-            )
+            is_star=is_star[ipl],
+        )
         planets.append(ith_pl_obj)
 
     # =============== CREATE STAR =============== #
-    star = Star(mass=st_m,radius=st_r)
+    star = Star(mass=st_m, radius=st_r)
     return DynamicSystem(star=star, planets=planets)
 
 
