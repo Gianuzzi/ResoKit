58f888241a5d7043a12ab8ecd6091031aea5a6d2
Gianuzzi <ema.gianuzzi@gmail.com>
2024-11-27 11:07:41 -0300

    Agregar archivo setup.py para la distribución e instalación de ResoKit, incluyendo dependencias y metadatos del paquete.

---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   ResoKit Project (https://github.com/Gianuzzi/resokit).
+# Copyright (c) 2024, Emmanuel Gianuzzi
+# License: MIT
+#   Full Text: https://github.com/Gianuzzi/resokit/blob/master/LICENSE
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""
+This file is for distribute and install ResoKit.
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import os
+import pathlib
+
+from setuptools import setup  # noqa
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+REQUIREMENTS = [
+    "numpy",
+    "matplotlib",
+    "pandas",
+    "scipy",
+]  # extras: " requests, astropy"
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PATH / "README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+with open(PATH / "resokit" / "__init__.py") as fp:
+    for line in fp.readlines():
+        if line.startswith("__version__ = "):
+            VERSION = line.split("=", 1)[-1].replace('"', "").strip()
+            break
+
+
+DESCRIPTION = "Miscellaneous tools for the analysis of planetary systems."
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+
+def do_setup():
+    setup(
+        name="resokit",
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        author=[
+            "Emmanuel Gianuzzi",
+            "Matías Cerioni",
+        ],
+        author_email="saraujo@iar.unlp.edu.ar",
+        url="https://github.com/Gianuzzi/resokit",
+        packages=[
+            "resokit",
+            "resokit.datasets",
+            "resokit.io",
+        ],
+        include_package_data=True,
+        license="MIT",
+        install_requires=REQUIREMENTS,
+        extras_require={"query": ["requests", "astropy"]},
+        keywords=["resokit", "planetary systems", "resonances"],
+        classifiers=[
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Scientific/Engineering",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    do_setup()
